a112450a9d41c81d43d95e017bc56f7e9ca519a8
Nickbis <nickbis3@gmail.com>
2022-12-24 01:20:29 +0300

    feat: all solution 02-numbers create

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -213,8 +213,12 @@ function roundToPowerOfTen(num, pow) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  if (n === 2 || n === 3) return true;
+  if (n <= 1 || n % 2 === 0 || n % 3 === 0) return false;
+  for (let i = 5; i * i <= n; i += 6) if (n % i === 0 || n % (i + 2) === 0) return false;
+  return true;
+  // throw new Error('Not implemented');
 }
 
 /**
@@ -232,8 +236,14 @@ function isPrime(/* n */) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  if (+value) {
+    return +value;
+  }
+
+  return def;
+
+  // throw new Error('Not implemented');
 }
 
 module.exports = {
