4104558fa71f270e2b7fdd950dc8472d9e2a116e
Nickbis <nickbis3@gmail.com>
2023-01-03 02:38:12 +0300

    feat: add solutions

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -173,8 +173,13 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let result = startFrom - 1;
+  return function nextNum() {
+    result += 1;
+    return result;
+  };
+  // throw new Error('Not implemented');
 }
 
 
